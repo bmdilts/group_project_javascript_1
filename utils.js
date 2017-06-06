682cb1b4bf59111ff57b6a2b90171789203d7273
Brandon <bmdilts@gmail.com>
2017-06-06 15:12:15 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -17,13 +17,13 @@ var findVowel = function(letter) {
 
     var vowels = ["a", "e", "i", "o", "u"];
 
-    for (var i = 0; i < vowels.length; i++) { // don't use for...in with Arrays
-        if (letter === vowels[i]) { // Use array indexing instead
+    for (var i = 0; i < vowels.length; i++) { 
+        if (letter === vowels[i]) { 
             return true;
         }
     }
 
-    return false; // This is after the loop
+    return false; 
 
 };
 // Function 4
